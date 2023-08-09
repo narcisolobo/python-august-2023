38a11c04f47c7d89d8dedd97e7791e2013dcb985
narcisolobo <narcisolobo@gmail.com>
2023-08-09 08:51:50 -0700

    w2d3 algo push

--- 00-algorithms/w2_strings_and_dicts/w2d3-braces-valid.js
+++ 00-algorithms/w2_strings_and_dicts/w2d3-braces-valid.js
@@ -4,13 +4,13 @@ Braces Valid
 Given a string sequence of parentheses, braces and brackets, determine whether it is valid. 
 */
 
-const str1 = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!";
+const str1 = 'W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!';
 const expected1 = true;
 
-const str2 = "D(i{a}l[ t]o)n{e";
+const str2 = 'D(i{a}l[ t]o)n{e';
 const expected2 = false;
 
-const str3 = "A(1)s[O (n]0{t) 0}k";
+const str3 = 'A(1)s[O (n]0{t) 0}k';
 const expected3 = false;
 
 /**
@@ -21,4 +21,25 @@ const expected3 = false;
  * @param {string} str
  * @returns {boolean} Whether the given strings braces are valid.
  */
-function bracesValid(str) {}
+function bracesValid(str) {
+  const stack = [];
+  const openers = '({[';
+  const closeToOpen = { ')': '(', '}': '{', ']': '[' };
+
+  for (const char of str) {
+    if (openers.includes(char)) {
+      stack.push(char);
+    } else if (char in closeToOpen) {
+      if (closeToOpen[char] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log(bracesValid(str1), 'should equal', expected1);
+console.log(bracesValid(str2), 'should equal', expected2);
+console.log(bracesValid(str3), 'should equal', expected3);
--- 00-algorithms/w2_strings_and_dicts/w2d3-parens-valid.js
+++ 00-algorithms/w2_strings_and_dicts/w2d3-parens-valid.js
@@ -5,27 +5,46 @@ Given an str that has parenthesis in it
 return whether the parenthesis are valid
 */
 
-const str1 = "Y(3(p)p(3)r)s";
+const str1 = 'Y(3(p)p(3)r)s';
 const expected1 = true;
 
-const str2 = "N(0(p)3";
+const str2 = 'N(0(p)3';
 const expected2 = false;
 // Explanation: not every parenthesis is closed.
 
-const str3 = "N(0)t ) 0(k";
+const str3 = 'N(0)t ) 0(k';
 const expected3 = false;
 // Explanation: because the second ")" is premature: there is nothing open for it to close.
 
-const str4 = "a(b))(c";
+const str4 = 'a(b))(c';
 const expected4 = false;
 // Explanation: same number of opens and closes but the 2nd closing closes nothing.
 
 /**
  * Determines whether the parenthesis in the given string are valid.
  * Each opening parenthesis must have exactly one closing parenthesis.
- * - Time: O(?).
- * - Space: O(?).
  * @param {string} str
  * @returns {boolean} Whether the parenthesis are valid.
  */
-function parensValid(str) {}
+function parensValid(str) {
+  const stack = [];
+
+  for (const char of str) {
+    if (char === '(') {
+      stack.push(char);
+    } else if (char === ')') {
+      if (stack.length === 0) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(parensValid(str1), 'should equal', expected1);
+console.log(parensValid(str2), 'should equal', expected2);
+console.log(parensValid(str3), 'should equal', expected3);
+console.log(parensValid(str4), 'should equal', expected4);
